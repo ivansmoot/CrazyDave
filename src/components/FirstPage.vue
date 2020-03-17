3a9fd7882fea2e074f1e139359f6778fd7d86c3b
ivansmoot <ivansmoot@163.com>
2020-03-17 19:59:22 +0800

    component kanban adds db now

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -34,8 +34,8 @@
     </div>
     <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
       <div class="my-3 p-3">
-        <h2 class="display-5">Another headline</h2>
-        <p class="lead" @click="toKanBan">And an even wittier subheading.</p>
+        <h2 class="display-5">Kanban</h2>
+        <el-link @click="toKanBan">click here to use a simple Kanban<i class="el-icon-view el-icon--right"></i> </el-link>
       </div>
       <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
     </div>
--- src/components/KanBan.vue
+++ src/components/KanBan.vue
@@ -1,12 +1,12 @@
 <template>
   <div id='kanban'>
-    <p style="text-align: center; margin: 0 0 20px; height: 50px">简单的KanBan</p>
+    <p style="text-align: center; margin: 0 0 20px; height: 50px">a simple KanBan</p>
     <div style="text-align: center">
       <el-transfer
         style="text-align: left; display: inline-block; height: 500px;"
         v-model="value"
         filterable
-        target-order="unshift"
+        target-order="original"
         :render-content="renderFunc"
         :titles="['BugList', '待测试']"
         :button-texts="['到左边', '到右边']"
@@ -23,13 +23,13 @@
           v-model="visible"
           cclass="transfer-footer"
           slot="left-footer">
-
-          <div style="margin: 20px 0;"></div>
           <el-input
             type="textarea"
             :autosize="{ minRows: 2, maxRows: 4}"
             placeholder="请输入内容"
-            v-model="textarea">
+            v-model="textarea"
+            style="position:absolute; left: 0; top: -45px"
+            @keyup.enter.native="pushdata()">
           </el-input>
 
           <div style="text-align: right; margin: 0">
@@ -38,7 +38,20 @@
           </div>
           <el-button slot="reference">+</el-button>
         </el-popover>
-        <el-button class="transfer-footer" slot="right-footer" size="small">操作</el-button>
+        <!-- <el-button class="transfer-footer" slot="right-footer" size="small">-</el-button> -->
+        <el-popover
+          placement="top"
+          width="160"
+          v-model="visible1"
+          cclass="transfer-footer"
+          slot="right-footer">
+            <p>这是一段内容这是一段内容确定删除吗？</p>
+            <div style="text-align: right; margin: 0">
+              <el-button size="mini" type="text" @click="visible1 = false">取消</el-button>
+              <el-button type="primary" size="mini" @click="visible1 = false">确定</el-button>
+            </div>
+          <el-button slot="reference">-</el-button>
+        </el-popover>
       </el-transfer>
     </div>
   </div>
@@ -64,40 +77,131 @@
 </style>
 
 <script>
+import { request } from '../network/request'
+import Vue from 'vue'
+import qs from 'qs'
+Vue.prototype.$qs = qs
 export default {
   name: 'kanban',
   data () {
-    const generateData = _ => {
-      const data = []
-      // for (let i = 1; i <= 15; i++) {
-      //   data.push({
-      //     key: i,
-      //     label: `备选项 ${i}`,
-      //     disabled: i % 4 === 0
-      //   })
-      // }
-      return data
-    }
     return {
-      data: generateData(),
-      value: [0],
+      data: [],
+      value: [], // 这玩意是保存哪些在右边的
       renderFunc (h, option) {
         return <span>{ option.key } - { option.label }</span>
       },
       visible: false,
+      visible1: false,
       textarea: '',
       keynum: 1
     }
   },
+  mounted () {
+    request({
+      url: '/kanban'
+    })
+      .then(res => {
+        this.data = res.data
+        for (let i = 0; i < res.data.length; i++) {
+          if (res.data[i].location === 'right') {
+            this.value.push(res.data[i].key)
+          }
+        }
+        // this.data = res.data
+        this.keynum = res.data.length + 1
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },
   methods: {
     handleChange (value, direction, movedKeys) {
-      console.log(value, direction, movedKeys)
+      if (direction === 'right') {
+        for (let i = 0; i < movedKeys.length; i++) {
+          for (let j = 0; j < this.data.length; j++) {
+            if (this.data[j].key === movedKeys[i]) {
+              const postData3 = this.$qs.stringify({
+                location: 'right'
+              })
+              request({
+                method: 'patch',
+                url: '/kanban/' + this.data[j].id,
+                data: postData3
+              }).then((res) => {
+                console.log(res)
+              })
+            }
+          }
+        }
+        // const postData3 = this.$qs.stringify({
+        //   location: 'right'
+        // })
+        // request({
+        //   method: 'patch',
+        //   url: '/kanban/WpcnDysnglibq8dLrtbjCF44Na0TGyB',
+        //   data: postData3
+        // }).then((res) => {
+        //   console.log(res)
+        // })
+      }
+      if (direction === 'left') {
+        for (let i = 0; i < movedKeys.length; i++) {
+          for (let j = 0; j < this.data.length; j++) {
+            if (this.data[j].key === movedKeys[i]) {
+              const postData4 = this.$qs.stringify({
+                location: 'left'
+              })
+              request({
+                method: 'patch',
+                url: '/kanban/' + this.data[j].id,
+                data: postData4
+              }).then((res) => {
+                console.log(res)
+              })
+            }
+          }
+        }
+      }
+    },
+    handleCurrentChange (currentPage) {
+      this.currentPage = currentPage
+    },
+    randomString (len) {
+      len = len || 32
+      var $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      var maxPos = $chars.length
+      var pwd = ''
+      for (let i = 0; i < len; i++) {
+        pwd += $chars.charAt(Math.floor(Math.random() * (maxPos + 1)))
+      }
+      return pwd
     },
     pushdata () {
       this.visible = false
-      this.data.push({
-        key: this.keynum++,
-        label: this.textarea
+      // this.data.push({
+      //   key: this.keynum++,
+      //   label: this.textarea
+      // })
+      const postData = this.$qs.stringify({
+        id: this.randomString(32),
+        key: this.keynum,
+        label: this.textarea,
+        location: 'left'
+      })
+      request({
+        method: 'post',
+        url: '/kanban',
+        data: postData
+      }).then((res) => {
+        console.log(res)
+      })
+      request({
+        url: '/kanban'
+      }).then(res => {
+        this.data = res.data
+        this.keynum = res.data.length + 1
+      }).catch(err => {
+        console.log(err)
       })
       this.textarea = null
     }
