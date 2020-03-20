e6800aea138a592b949e4a5ec1470b30b7d60cc5
ivansmoot <ivansmoot@163.com>
2020-03-20 19:46:19 +0800

    component ranjin works better

--- src/components/RanJin.vue
+++ src/components/RanJin.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="ranjin">
     <v-chart ref="chart1" :options="orgOptions" :auto-resize="true"></v-chart>
+    <div style="text-align: center">
+      <span >请选择项目起止时间</span>
+      <p></p>
+      <el-date-picker
+        v-model="value1"
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        value-format="yyyy-MM-dd"
+        style="width: 400px">
+      </el-date-picker>
+      <P></P>
+      <el-button type="primary" round @click="showdate">绘制理想燃尽图</el-button>
+    </div>
   </div>
 </template>
 
@@ -17,7 +32,11 @@ export default {
   name: 'ranjin',
   data () {
     return {
-      orgOptions: {}
+      orgOptions: {},
+      value1: '',
+      xData: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      yData: [820, 932, 901, 934, 1290, 1330, 1320],
+      yData2: [920, 1032, 1001, 1034, 1390, 1430, 1420]
     }
   },
   mounted () {
@@ -28,17 +47,40 @@ export default {
       this.orgOptions = {
         xAxis: {
           type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+          data: this.xData
         },
         yAxis: {
           type: 'value'
         },
         series: [{
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          data: this.yData,
+          type: 'line',
+          smooth: true
+        },
+        {
+          data: this.yData2,
           type: 'line',
           smooth: true
         }]
       }
+    },
+    showdate () {
+      this.xData = []
+      this.yData = []
+      const fday = parseInt(this.value1[0].substr(8, 2))
+      const lday = parseInt(this.value1[1].substr(8, 2))
+      for (let i = fday; i <= lday; i++) {
+        this.xData.push(i)
+      }
+      this.yData.push('100')
+      let ydata = 100
+      const stride = 100 / (lday - fday)
+      for (let j = fday + 1; j < lday; j++) {
+        ydata -= stride
+        this.yData.push(ydata)
+      }
+      this.yData.push('0')
+      this.drawIt()
     }
   }
 }
