21da9b0c9f93713ce54a094a9453c3ddbb9ce83f
ivansmoot <ivansmoot@163.com>
2020-03-20 20:20:11 +0800

    component ranjin works better

--- src/components/RanJin.vue
+++ src/components/RanJin.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="ranjin">
     <v-chart ref="chart1" :options="orgOptions" :auto-resize="true"></v-chart>
-    <div style="text-align: center">
-      <span >请选择项目起止时间</span>
-      <p></p>
-      <el-date-picker
-        v-model="value1"
-        type="daterange"
-        range-separator="至"
-        start-placeholder="开始日期"
-        end-placeholder="结束日期"
-        value-format="yyyy-MM-dd"
-        style="width: 400px">
-      </el-date-picker>
-      <P></P>
-      <el-button type="primary" round @click="showdate">绘制理想燃尽图</el-button>
+    <div style="position: relative; height: 200px">
+      <div style="position: absolute; width: 400px; left: 250px; top: 50px">
+        <span style="position: absolute; left: 100px; top: -30px">请选择项目起止时间</span>
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          value-format="yyyy-MM-dd"
+          style="width: 380px">
+        </el-date-picker>
+        <el-button type="primary" round @click="showdate"
+          style="position: absolute; left: 100px; top: 50px">绘制理想燃尽图</el-button>
+      </div>
+      <div style="position: absolute; width: 200px; left: 750px; top: 50px">
+        <span style="position: absolute; left: 155px; top: -30px; width: 200px">请输入当前进度</span>
+        <el-input
+          placeholder="请输入日期(格式:2020-03-20)"
+          suffix-icon="el-icon-date"
+          v-model="input1"
+          style="position: absolute; width:240px; left: -40px">
+        </el-input>
+        <el-input
+          placeholder="请输入剩余进度"
+          prefix-icon="el-icon-search"
+          v-model="input2"
+          style="position: absolute; left: 230px">
+        </el-input>
+        <el-button type="primary" round @click="showdate"
+          style="position: absolute; left: 140px; top: 50px">绘制当前燃尽图</el-button>
+      </div>
     </div>
   </div>
 </template>
@@ -36,7 +54,9 @@ export default {
       value1: '',
       xData: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
       yData: [820, 932, 901, 934, 1290, 1330, 1320],
-      yData2: [920, 1032, 1001, 1034, 1390, 1430, 1420]
+      yData2: [920, 1032, 1001, 1034, 1390, 1430, 1420],
+      input1: '',
+      input2: ''
     }
   },
   mounted () {
